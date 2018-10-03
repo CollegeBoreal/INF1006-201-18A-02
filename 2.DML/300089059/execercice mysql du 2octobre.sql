48cfb1d1b201e70fd4fd7e7bdfc92f8c51e37384
fabienpaul <31968886+fabienpaul@users.noreply.github.com>
2018-10-03 13:43:55 -0400

    Add files via upload

---
+++ 2.DML/300089059/execercice mysql du 2octobre.sql
@@ -0,0 +1,3 @@
+SELECT name, sum (amout) FROM customer city , payment "city" where "lubumbashi";
+select name, amount from country = "Congo" and "Algerie";
+select name,sum (amount) FROM country = "Congo" and "Algerie";
